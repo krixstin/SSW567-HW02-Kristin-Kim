a10979d00a48248698a850d8ff6379c41a846aaf
krixstin <kristinhajinkim@gmail.com>
2020-09-16 19:02:35 -0400

    change commit sample

--- TestTriangle.py
+++ TestTriangle.py
@@ -18,13 +18,19 @@ class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
     def testRightTriangleA(self): 
-        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+        self.assertEqual(classifyTriangle(3,4,5),'Right',msg='3,4,5 is a Right triangle')
 
     def testRightTriangleB(self): 
-        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
+        self.assertEqual(classifyTriangle(5,3,4),'Right',msg='5,3,4 is a Right triangle')
         
     def testEquilateralTriangles(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+        self.assertEqual(classifyTriangle(1,1,1),'Equilateral',msg='1,1,1 should be equilateral')
+
+    def testScaleneTriangles(self):
+        self.assertEqual(classifyTriangle(5,7,8),'Scalene',msg='5,7,8 should be scalene')
+
+    def testIsocelesTriangles(self):
+        self.assertEqual(classifyTriangle(5,4,4),'Isoceles',msg='5,4,4 should be isoceles')
 
 if __name__ == '__main__':
     print('Running unit tests')
