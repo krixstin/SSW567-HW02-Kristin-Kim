23bdfc512cb7e3a78edd1cc14a1215858d57797e
krixstin <kristinhajinkim@gmail.com>
2020-10-07 02:35:21 -0400

    HW05 analyzed

--- Triangle.py
+++ Triangle.py
@@ -9,7 +9,7 @@ The primary goal of this file is to demonstrate a simple python program to class
 @author: rk
 """
 
-def classifyTriangle(a,b,c):
+def classifyTriangle(a, b, c):
     """
     Your correct code goes here...  Fix the faulty logic below until the code passes all of 
     you test cases. 
@@ -36,8 +36,8 @@ def classifyTriangle(a,b,c):
     #
     # # verify that all 3 inputs are integers
     # # Python's "isinstance(object,type) returns True if the object is of the specified type
-    elif not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
-        return 'InvalidInput';
+    elif not(isinstance(a, int) and isinstance(b, int) and isinstance(c, int)):
+        return 'InvalidInput'
       
     # This information was not in the requirements spec but 
     # is important for correctness
@@ -50,9 +50,9 @@ def classifyTriangle(a,b,c):
     # now we know that we have a valid triangle 
         if a == b and b == c:
             return 'Equilateral'
-        elif ((a ^2) + (b ^ 2)) == (c ^ 2):
+        elif ((a ^ 2) + (b ^ 2)) == (c ^ 2):
             return 'Right'
-        elif a==b or a==c or b==c:
+        elif a == b or a == c or b == c:
             return 'Isosceles'
         else:
             return "Scalene"
